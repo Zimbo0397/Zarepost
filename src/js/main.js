dff868b0b4adf86b5929b3c2c46ddb9c987e7992
Zimbo0397 <zimbo0397@Gmail.com>
2017-02-09 12:42:49 +0200

    fixes 09/02/2017

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,29 @@
 
 $(document).ready(function(){
-  $('.bxslider').bxSlider();
+  $('.bxslider').bxSlider({
+		minSlides: 1,
+		maxSlides: 3,
+		slideWidth: 260,
+		slideMargin: 10
+  });
+});
+
+$('#mobile-nav-btn').on('click', function() {
+	$('.mobile-nav-holder').toggleClass('open');
+});
+
+$('.acc-box').on('click', function(e) {
+	e.preventDefault();
+	$('.profile-panell').addClass('open');
+	$('.black-plate').addClass('active');
+	$('body').addClass('b-overflow');
+});
+
+$('.close-btn').each(function() {
+	$(this).on('click', function(e) {
+		e.preventDefault();
+		$('.profile-panell').removeClass('open');
+		$('.black-plate').removeClass('active');
+		$('body').removeClass('b-overflow');
+	});
 });
